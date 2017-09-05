f3a14655b0092c5ee07989d2d9bb23b9566dbad9
Mattia Iavarone <mat.iavarone@gmail.com>
2017-09-05 14:25:33 +0200

    Weak cache for threads

--- cameraview/src/main/utils/com/otaliastudios/cameraview/WorkerHandler.java
+++ cameraview/src/main/utils/com/otaliastudios/cameraview/WorkerHandler.java
@@ -3,12 +3,33 @@ package com.otaliastudios.cameraview;
 import android.os.Handler;
 import android.os.HandlerThread;
 
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Class holding a background handler.
- * Using setDaemon right now but a better approach would be to have
- * start() and stop() callbacks here. TODO
+ * We want them to survive configuration changes if there's still job to do.
  */
 class WorkerHandler {
+
+    private static ConcurrentHashMap<String, WeakReference<WorkerHandler>> sCache = new ConcurrentHashMap<>(4);
+
+    public static WorkerHandler get(String name) {
+        if (sCache.containsKey(name)) {
+            WorkerHandler cached = sCache.get(name).get();
+            if (cached != null) {
+                HandlerThread thread = cached.mThread;
+                if (thread.isAlive() && !thread.isInterrupted()) {
+                    return cached;
+                }
+            }
+            sCache.remove(name);
+        }
+        WorkerHandler handler = new WorkerHandler(name);
+        sCache.put(name, new WeakReference<>(handler));
+        return handler;
+    }
 
     private HandlerThread mThread;
     private Handler mHandler;
