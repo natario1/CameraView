08889d59ac86407d3ecf22c090e444fd05729c9c
Mattia Iavarone <mat.iavarone@gmail.com>
2017-11-14 15:28:56 +0100

    AspectRatio tests

--- cameraview/src/test/java/com/otaliastudios/cameraview/AspectRatioTest.java
+++ cameraview/src/test/java/com/otaliastudios/cameraview/AspectRatioTest.java
@@ -50,4 +50,21 @@ public class AspectRatioTest {
         assertEquals(inverse.getX(), 1f, 0);
         assertEquals(inverse.getY(), 5f, 0);
     }
+
+    @Test(expected = NumberFormatException.class)
+    public void testParse_notNumbers() {
+        AspectRatio.parse("a:b");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testParse_noColon() {
+        AspectRatio.parse("24");
+    }
+
+    @Test
+    public void testParse() {
+        AspectRatio ratio = AspectRatio.parse("16:9");
+        assertEquals(ratio.getX(), 16);
+        assertEquals(ratio.getY(), 9);
+    }
 }
