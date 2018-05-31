810323aefa7f0c7a1a5e7bda04c1dc799fd7f32d
Josh Burton <athor3d@gmail.com>
2018-06-01 07:16:21 +1200

    Provides synchronous version of decodeBitmap and adds annotation to make it clear this should run on a worker thread
    Closes #221

--- cameraview/src/main/utils/com/otaliastudios/cameraview/CameraUtils.java
+++ cameraview/src/main/utils/com/otaliastudios/cameraview/CameraUtils.java
@@ -8,6 +8,7 @@ import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.os.Handler;
 import android.support.annotation.UiThread;
+import android.support.annotation.WorkerThread;
 import android.support.media.ExifInterface;
 
 import java.io.ByteArrayInputStream;
@@ -62,6 +63,20 @@ public class CameraUtils {
      * This is executed in a background thread, and returns the result to the original thread.
      *
      * @param source a JPEG byte array
+     */
+    @SuppressWarnings("WeakerAccess")
+    @WorkerThread
+    public static void decodeBitmap(final byte[] source) {
+        decodeBitmap(source, Integer.MAX_VALUE, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Decodes an input byte array and outputs a Bitmap that is ready to be displayed.
+     * The difference with {@link android.graphics.BitmapFactory#decodeByteArray(byte[], int, int)}
+     * is that this cares about orientation, reading it from the EXIF header.
+     * This is executed in a background thread, and returns the result to the original thread.
+     *
+     * @param source a JPEG byte array
      * @param callback a callback to be notified
      */
     @SuppressWarnings("WeakerAccess")
@@ -102,7 +117,8 @@ public class CameraUtils {
 
     // TODO ignores flipping
     @SuppressWarnings({"SuspiciousNameCombination", "WeakerAccess"})
-    /* for tests */ static Bitmap decodeBitmap(byte[] source, int maxWidth, int maxHeight) {
+    @WorkerThread
+    public static Bitmap decodeBitmap(byte[] source, int maxWidth, int maxHeight) {
         if (maxWidth <= 0) maxWidth = Integer.MAX_VALUE;
         if (maxHeight <= 0) maxHeight = Integer.MAX_VALUE;
         int orientation;
