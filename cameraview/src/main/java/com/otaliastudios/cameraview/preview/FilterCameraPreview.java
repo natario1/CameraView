5f6109040638b86959ec24e509177da81bc3b301
Mattia Iavarone <mat.iavarone@gmail.com>
2019-08-05 16:13:50 +0200

    Add comments

--- cameraview/src/main/java/com/otaliastudios/cameraview/preview/FilterCameraPreview.java
+++ cameraview/src/main/java/com/otaliastudios/cameraview/preview/FilterCameraPreview.java
@@ -12,15 +12,28 @@ import com.otaliastudios.cameraview.filters.Filter;
 
 /**
  * A preview that support GL filters defined through the {@link Filter} interface.
+ *
+ * The preview has the responsibility of calling {@link Filter#setPreviewingViewSize(int, int)}
+ * whenever the preview size changes and as soon as the filter is applied.
  */
 public abstract class FilterCameraPreview<T extends View, Output> extends CameraPreview<T, Output> {
 
+    @SuppressWarnings("WeakerAccess")
     public FilterCameraPreview(@NonNull Context context, @NonNull ViewGroup parent) {
         super(context, parent);
     }
 
+    /**
+     * Sets a new filter.
+     * @param filter new filter
+     */
     public abstract void setFilter(@NonNull Filter filter);
 
+    /**
+     * Returns the currently used filter.
+     * @return currently used filter
+     */
+    @SuppressWarnings("unused")
     @NonNull
     public abstract Filter getCurrentFilter();
 }
