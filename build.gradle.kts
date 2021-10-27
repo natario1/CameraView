9d1306448e67601b5f7c7355c2240b197e59b26f
Mattia Iavarone <mat.iavarone@gmail.com>
2021-10-27 18:56:15 +0200

    Update Kotlin, Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 buildscript {
 
     extra["minSdkVersion"] = 15
-    extra["compileSdkVersion"] = 30
-    extra["targetSdkVersion"] = 30
+    extra["compileSdkVersion"] = 31
+    extra["targetSdkVersion"] = 31
 
     repositories {
         google()
@@ -11,9 +11,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("com.android.tools.build:gradle:7.0.3")
         classpath("io.deepmedia.tools:publisher:0.6.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
 
     }
 }
